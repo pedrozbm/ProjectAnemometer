a7d34c9fa7c794e263f747b389a03da45f43cb9b
Pedro Barbieri <112345954+pedrozbm@users.noreply.github.com>
2024-05-19 19:17:37 -0300

    Update Anemometro.ino
    Pelo menos agora tá contando

--- Anemometro.ino
+++ Anemometro.ino
@@ -1,43 +1,49 @@
-//Programa: Sensor de velocidade Arduino LM393
-//Autor: Arduino e Cia
-//Pino ligado ao pino D0 do sensor
-int pino_D0 = 2;
-int rpm;
-volatile byte pulsos;
+
+#define pinoLed 13
+#define PinoSensor 15
+
+int estadoOn = 0;
+int estadoOff = 0;
+int cont = 0;   
+int aux = 0;
+unsigned long millisContador = millis();
+volatile bool estado = false;
 unsigned long timeold;
-//Altere o numero abaixo de acordo com o seu disco encoder
-unsigned int pulsos_por_volta = 20;
-void contador()
-{
-  //Incrementa contador
-  pulsos++;
+
+
+void setup() {
+
+Serial.begin(115200);
+pinMode(PinoSensor, INPUT);
+pinMode(pinoLed, OUTPUT);
 }
-void setup()
-{
-  Serial.begin(9600);
-  //Pino do sensor como entrada
-  pinMode(pino_D0, INPUT);
-  //Interrupcao 0 - pino digital 2
-  //Aciona o contador a cada pulso
-  attachInterrupt(0, contador, FALLING);
-  pulsos = 0;
-  rpm = 0;
-  timeold = 0;
-}
-void loop()
-{
-  //Atualiza contador a cada segundo
-  if (millis() - timeold >= 1000)
-  {
-    //Desabilita interrupcao durante o calculo
-    detachInterrupt(0);
-    rpm = (60 * 1000 / pulsos_por_volta ) / (millis() - timeold) * pulsos;
-    timeold = millis();
-    pulsos = 0;
-    //Mostra o valor de RPM no serial monitor
-    Serial.print("RPM = ");
-    Serial.println(rpm, DEC);
-    //Habilita interrupcao
-    attachInterrupt(0, contador, FALLING);
+
+void loop() {
+  
+  if(digitalRead(PinoSensor)==HIGH){
+    estadoOn = 1;
+  }
+  else{
+    estadoOff = 1;
+  }
+  if(estadoOn == estadoOff){
+    cont = cont + 1; 
+    Serial.println(cont);
+    estadoOn = 0;
+    estadoOff = 0;
   }
 }
+
+
+/*
+if (millis() - timeold >= 1000){
+  if(digitalRead(PinoSensor)==HIGH){      
+      digitalWrite(pinoLed, HIGH);
+      detachInterrupt(digitalPinToInterrupt(PinoSensor));
+
+  }else{
+    digitalWrite(pinoLed, LOW);
+    Serial.println(cont);
+  }  
+}
+}*/
