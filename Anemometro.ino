588bedf37d4d6f09af2cbf95f23bee84a54a5b40
Pedro Barbieri <112345954+pedrozbm@users.noreply.github.com>
2024-05-21 21:36:49 -0300

    Update Anemometro.ino
    Nova base para o código desenvolvido pelo professor Kendy (Não funcionou porra nenhuma também )

--- Anemometro.ino
+++ Anemometro.ino
@@ -1,49 +1,36 @@
-
 #define pinoLed 13
-#define PinoSensor 15
+#define PinoSensor 23
 
-int estadoOn = 0;
-int estadoOff = 0;
-int cont = 0;   
-int aux = 0;
-unsigned long millisContador = millis();
+int cont = 0;
 volatile bool estado = false;
 unsigned long timeold;
 
+void IRAM_ATTR handleInterrupt() {
+  estado = !estado; // Alterna o estado sempre que uma interrupção ocorre
+  //cont++;
+  //Serial.println(cont);
+}
 
 void setup() {
-
-Serial.begin(115200);
-pinMode(PinoSensor, INPUT);
-pinMode(pinoLed, OUTPUT);
+  Serial.begin(115200);
+  pinMode(PinoSensor, INPUT);
+  pinMode(pinoLed, OUTPUT);
+  attachInterrupt(digitalPinToInterrupt(PinoSensor), handleInterrupt, RISING); //RISING
 }
 
 void loop() {
-  
-  if(digitalRead(PinoSensor)==HIGH){
-    estadoOn = 1;
-  }
-  else{
-    estadoOff = 1;
-  }
-  if(estadoOn == estadoOff){
-    cont = cont + 1; 
-    Serial.println(cont);
-    estadoOn = 0;
-    estadoOff = 0;
-  }
-}
-
-
-/*
-if (millis() - timeold >= 1000){
-  if(digitalRead(PinoSensor)==HIGH){      
+  if (millis() - timeold >= 1000) {
+    if (estado) { // Se o estado for verdadeiro, acende o LED
       digitalWrite(pinoLed, HIGH);
-      detachInterrupt(digitalPinToInterrupt(PinoSensor));
+      
+       //Serial.println("Kendy: "+cont);
+    } else { // Se o estado for falso, apaga o LED
+      digitalWrite(pinoLed, LOW);
+    }
+    timeold = millis(); // Atualiza o tempo
+   // Serial.println(cont);
+    cont++;
+  }
+  Serial.println(cont);
 
-  }else{
-    digitalWrite(pinoLed, LOW);
-    Serial.println(cont);
-  }  
 }
-}*/
