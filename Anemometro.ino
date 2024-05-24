b0a90f1b96fce9cca922e16c415a4f9d4b717994
Pedro Barbieri <112345954+pedrozbm@users.noreply.github.com>
2024-05-24 12:08:52 -0300

    Update Anemometro.ino
    Algumas alterações baseadas no código do Fernando K : https://www.fernandok.com/2018/06/os-profissionais-sabem-disso-interrupt.html

--- Anemometro.ino
+++ Anemometro.ino
@@ -1,25 +1,35 @@
+
 #define pinoLed 13
 #define PinoSensor 23
 
-int cont = 0;
-volatile bool estado = false;
-unsigned long timeold;
 
-void IRAM_ATTR handleInterrupt() {
-  if(estado = !estado){ // Alterna o estado sempre que uma interrupção ocorre
-  cont++;
-  }
-  //Serial.println(cont);
+volatile int cont = 0;
+volatile bool laststate;
+volatile uint32_t debounceTimeout = 0;
+
+uint32_t saveDebounceTimeout;
+bool saveLastState;
+int save;
+
+void IRAM_ATTR SensorInterrupt() {
+
+portENTER_CRITICAL_ISR(&mux); 
+    cont++;
+    lastState = digitalRead(PinoSensor);  
+    debounceTimeout = xTaskGetTickCount(); //versão do millis () que funciona a partir da interrupção //version of millis() that works from interrupt
+    portEXIT_CRITICAL_ISR(&mux);
 }
 
 void setup() {
   Serial.begin(115200);
   pinMode(PinoSensor, INPUT);
   pinMode(pinoLed, OUTPUT);
-  attachInterrupt(digitalPinToInterrupt(PinoSensor), handleInterrupt, FALLING); //RISING
+  attachInterrupt(digitalPinToInterrupt(PinoSensor), SensorInterrupt, FALLING); //RISING
+  
 }
 
 void loop() {
+  /*
   if (millis() - timeold >= 1000) {
     if (estado) { // Se o estado for verdadeiro, acende o LED
       digitalWrite(pinoLed, HIGH);
@@ -30,7 +40,16 @@ void loop() {
     }
     timeold = millis(); // Atualiza o tempo
    // Serial.println(cont);
-   }
+   }*/
+   portENTER_CRITICAL_ISR(&mux); // início da seção crítica
+      save  = cont;
+      saveDebounceTimeout = debounceTimeout;
+      saveLastState  = lastState;
+  portEXIT_CRITICAL_ISR(&mux); 
+ bool currentState = digitalRead(PinoSensor);
+  if(currentState != saveLastState)
+    {
+      saveDebounceTimeout = millis();
+    }
   Serial.println(cont);
-
 }
