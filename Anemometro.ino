7a9c8c1c7f51be89df30deeef850600388bcc7e3
Pedro Barbieri <112345954+pedrozbm@users.noreply.github.com>
2024-05-27 21:34:23 -0300

    Update Anemometro.ino
    Agora o sensor consegue contar os pulsos HIGH

--- Anemometro.ino
+++ Anemometro.ino
@@ -1,56 +1,38 @@
-
 #define pinoLed 13
 #define PinoSensor 23
 
+int estadoOn = 0;
+int estadoOff = 0;
+int cont = 0;   
+int aux = 0;
+unsigned long millisContador = millis();
+volatile bool estado = false;
 
-volatile int cont = 0;
-volatile bool lastState;
-volatile uint32_t debounceTimeout = 0;
-
-uint32_t saveDebounceTimeout;
-bool saveLastState;
-int save;
-portMUX_TYPE mux = portMUX_INITIALIZER_UNLOCKED;
 
 void IRAM_ATTR SensorInterrupt() {
-
-portENTER_CRITICAL_ISR(&mux); 
-    cont++;
-    lastState = digitalRead(PinoSensor);  
-    debounceTimeout = xTaskGetTickCount(); //versão do millis () que funciona a partir da interrupção //version of millis() that works from interrupt
-    portEXIT_CRITICAL_ISR(&mux);
+if(estadoOn == 1 && estadoOff == 1){
+    cont = cont + 1; 
+    estadoOn = 0;
+    estadoOff = 0;    
+  }
 }
 
 void setup() {
-  Serial.begin(115200);
-  pinMode(PinoSensor, INPUT);
-  pinMode(pinoLed, OUTPUT);
-  attachInterrupt(digitalPinToInterrupt(PinoSensor), SensorInterrupt, FALLING); //RISING
-  
+
+Serial.begin(115200);
+pinMode(PinoSensor, INPUT);
+pinMode(pinoLed, OUTPUT);
+attachInterrupt(digitalPinToInterrupt(PinoSensor), SensorInterrupt, RISING); //RISING
+
 }
 
+
 void loop() {
-  /*
-  if (millis() - timeold >= 1000) {
-    if (estado) { // Se o estado for verdadeiro, acende o LED
-      digitalWrite(pinoLed, HIGH);
-      
-       //Serial.println("Kendy: "+cont);
-    } else { // Se o estado for falso, apaga o LED
-      digitalWrite(pinoLed, LOW);
-    }
-    timeold = millis(); // Atualiza o tempo
-   // Serial.println(cont);
-   }*/
-   portENTER_CRITICAL_ISR(&mux); // início da seção crítica
-      save  = cont;
-      saveDebounceTimeout = debounceTimeout;
-      saveLastState  = lastState;
-  portEXIT_CRITICAL_ISR(&mux); 
- bool currentState = digitalRead(PinoSensor);
-  if(currentState != saveLastState)
-    {
-      saveDebounceTimeout = millis();
-    }
+
+  if(digitalRead(PinoSensor)==HIGH){
+    estadoOn = 1;
+    estadoOff = 1;
+  }
   Serial.println(cont);
+  
 }
